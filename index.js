99c0f2c4e1119d72a5d1bcd6e925cf9b6900ae6e
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-10 11:53:41 -0800

    commit tree 8f45d6e31d1b7a1bf388d5e7c6c24154129ce0d4

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function writeCards(names,event) { 
-    let messages = [];
-    for (let i = 0; i < names.length; i++){ 
-    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-  }
-return messages; 
+function writeCards(names,event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++){
+  messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+}
+return messages;
 }
 
 function countDown(positiveNumber) {
-   while(positiveNumber >= 0){
-    console.log(positiveNumber);
-    positiveNumber--;
-    }
+ while(positiveNumber >= 0){
+  console.log(positiveNumber);
+  positiveNumber--;
+  }
 }
